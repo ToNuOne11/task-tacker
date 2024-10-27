14034c1ba86c34e5dec2af712a27d0ee9dda42b6
Sergey Shadrin <juuute3110@gmail.com>
2024-10-27 14:10:05 +0700

    add EditProject function

--- src/main/java/ru/shadrinsa/task_tracker_api/api/controllers/ProjectController.java
+++ src/main/java/ru/shadrinsa/task_tracker_api/api/controllers/ProjectController.java
@@ -2,14 +2,15 @@ package ru.shadrinsa.task_tracker_api.api.controllers;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.shadrinsa.task_tracker_api.api.dto.ProjectDto;
 import ru.shadrinsa.task_tracker_api.api.exeptions.BadRequestException;
+import ru.shadrinsa.task_tracker_api.api.exeptions.NotFoundException;
 import ru.shadrinsa.task_tracker_api.api.factories.ProjectDtoFactory;
 import ru.shadrinsa.task_tracker_api.store.entities.ProjectEntity;
 import ru.shadrinsa.task_tracker_api.store.repositories.ProjectRepository;
+
+import java.util.Objects;
 
 @Transactional
 @RestController
@@ -20,9 +21,15 @@ public class ProjectController {
     final private ProjectRepository projectRepository;
 
     public static final String CREATE_PROJECT = "/api/projects";
+    public static final String EDIT_PROJECT = "/api/projects/{project_id}";
+
 
     @PostMapping(CREATE_PROJECT)
     public ProjectDto createProject(@RequestParam String name){
+        if(name.trim().isEmpty()){
+            throw new BadRequestException("name is empty.");
+        }
+
         projectRepository
                 .findByName(name)
                 .ifPresent(project -> {
@@ -33,6 +40,29 @@ public class ProjectController {
                         .name(name)
                         .build()
         );
+        return projectDtoFactory.makeProjectDto(project);
+    }
+    @PatchMapping(EDIT_PROJECT)
+    public ProjectDto editProject(@PathVariable Long project_id, @RequestParam String name){
+        if(name.trim().isEmpty()){
+            throw new BadRequestException("name is empty.");
+        }
+
+        ProjectEntity project = projectRepository
+                .findById(project_id)
+                .orElseThrow(() ->
+                        new NotFoundException("Project not found")
+                );
+
+        projectRepository
+                .findByName(name)
+                .filter(anotherProject -> !Objects.equals(anotherProject.getId(), project_id))
+                .ifPresent(anotherProject -> {
+                    throw new BadRequestException("Project already exists.");
+                });
+
+        project = projectRepository.saveAndFlush(project);
+
         return projectDtoFactory.makeProjectDto(project);
     }
 }
