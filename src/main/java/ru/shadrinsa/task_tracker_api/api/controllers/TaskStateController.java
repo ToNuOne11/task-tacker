36ca85f2acaf1cd78adb41337d784c909b505133
Sergey Shadrin <juuute3110@gmail.com>
2024-10-28 17:47:50 +0700

    fix createTaskState

--- src/main/java/ru/shadrinsa/task_tracker_api/api/controllers/TaskStateController.java
+++ src/main/java/ru/shadrinsa/task_tracker_api/api/controllers/TaskStateController.java
@@ -12,6 +12,7 @@ import ru.shadrinsa.task_tracker_api.store.entities.TaskStateEntity;
 import ru.shadrinsa.task_tracker_api.store.repositories.TaskStateRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @RestController
@@ -27,7 +28,7 @@ public class TaskStateController {
 
 
     @GetMapping(GET_TASK_STATES)
-    public List<TaskStateDto> getTaskStates(@PathVariable(name = "project_id") Long projectId){
+    public List<TaskStateDto> getTaskStates(@PathVariable(name = "project_id") Long projectId) {
 
         ProjectEntity project = controllerHelper.getProjectOrThrowException(projectId);
 
@@ -44,37 +45,42 @@ public class TaskStateController {
         if (taskStateName.trim().isEmpty()) {
             throw new BadRequestException("Task state name is empty");
         }
-
         ProjectEntity project = controllerHelper.getProjectOrThrowException(projectId);
-        project
-                .getTaskStates()
-                .stream()
-                .map(TaskStateEntity::getName)
-                .filter(anotherTaskStateName -> anotherTaskStateName.equalsIgnoreCase(taskStateName))
-                .findAny()
-                .ifPresent(it -> {
-                    throw new BadRequestException("Task state already exists.");
-                });
+
+        Optional<TaskStateEntity> optionalAnotherTaskState = Optional.empty();
+
+        for (TaskStateEntity taskState : project.getTaskStates()) {
+            if (taskState.getName().equalsIgnoreCase(taskStateName)) {
+                throw new BadRequestException("Task state already exists.");
+            }
+            if (!taskState.getRightTaskState().isPresent()) {
+                optionalAnotherTaskState = Optional.of(taskState);
+                break;
+            }
+        }
+
 
         final TaskStateEntity taskState = taskStateRepository.saveAndFlush(
                 TaskStateEntity.builder()
                         .name(taskStateName)
+                        .project(project)
                         .build()
-                );
+        );
 
-        taskStateRepository
-                .findTaskStateEntityByRightTaskStateIdIsNullAndProjectId(projectId)
+        optionalAnotherTaskState
                 .ifPresent(anotherTaskState -> {
-                    taskState.setLeftTaskState(taskState);
+                    taskState.setLeftTaskState(anotherTaskState);
 
                     anotherTaskState.setRightTaskState(taskState);
 
-                    taskStateRepository.saveAndFlush(anotherTaskState);
+                    taskStateRepository.saveAndFlush(taskState);
                 });
 
         final TaskStateEntity savedTaskState = taskStateRepository.saveAndFlush(taskState);
 
-        return taskStateDtoFactory.makeTaskStateDto(savedTaskState );
+        return taskStateDtoFactory.makeTaskStateDto(savedTaskState);
     }
-
 }
+
+
+
--- src/main/java/ru/shadrinsa/task_tracker_api/store/repositories/TaskStateRepository.java
+++ src/main/java/ru/shadrinsa/task_tracker_api/store/repositories/TaskStateRepository.java
@@ -3,8 +3,7 @@ package ru.shadrinsa.task_tracker_api.store.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.shadrinsa.task_tracker_api.store.entities.TaskStateEntity;
 
-import java.util.Optional;
 
 public interface TaskStateRepository extends JpaRepository<TaskStateEntity, Long> {
-    Optional<TaskStateEntity> findTaskStateEntityByRightTaskStateIdIsNullAndProjectId(Long projectId);
+
 }
